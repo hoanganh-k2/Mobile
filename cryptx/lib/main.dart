d93b870229242be6b71f069b322d82f6c16a12df
blue552 <22010697@st.phenikaa-uni.edu.vn>
2025-03-22 17:29:06 +0700

    Up code

---
+++ cryptx/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:wallet/providers/ethereum_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/login_screen.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  await dotenv.load();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => EthereumProvider(
+              dotenv.env['RPC_URL'] ?? 'http://127.0.0.1:7545', http.Client()),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Wallet',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
